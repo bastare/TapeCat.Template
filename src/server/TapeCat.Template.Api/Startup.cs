78071931e72fa23eb4baa0943064d7710e1307a3
Yaroslav Fesiuk <38162485+bastare@users.noreply.github.com>
2024-06-24 22:36:10 -0400

    Sync with template

--- src/server/TapeCat.Template.Api/Startup.cs
+++ src/server/TapeCat.Template.Api/Startup.cs
@@ -188,11 +188,18 @@ public sealed class Startup ( IConfiguration configuration , IWebHostEnvironment
 			.UseExceptionHandler ( GlobalExceptionHandlerConfigurator.ExceptionFiltersConfigurator )
 			.UseEndpoints ( endpoints =>
 			{
+				/*
+					! If swagger doesn't work (white screen):
+					1. Comment this 2 lines (Yarp mapping)
+					2. Launch server & manually move to swagger page (http://localhost:5000/swagger/index.html)
+					3. Uncomment it back
+					4. Launch server again & don't close previously opened tab with swagger (step 1)
+				*/
 				if ( WebHostEnvironmentExtensions.IsDevelopment ( _webHostEnvironment ) )
 					endpoints.MapSpaYarp ();
 
-				endpoints.MapFallbackToFile (
-					filePath: Path.Combine ( _webHostEnvironment.WebRootPath , "index.html" ) );
+				if ( WebHostEnvironmentExtensions.IsProduction ( _webHostEnvironment ) )
+					endpoints.MapFallbackToFile ( "index.html" );
 
 				endpoints.MapFastEndpoints ();
 
