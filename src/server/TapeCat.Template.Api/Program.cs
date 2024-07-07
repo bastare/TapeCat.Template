2cdabebe0f4c1ae43c3a2ffd1ead0f7df17f149b
reds <yaroslav.fesiuk@tape-cat.com>
2024-07-07 03:13:23 -0400

    Refactoring

--- src/server/TapeCat.Template.Api/Program.cs
+++ src/server/TapeCat.Template.Api/Program.cs
@@ -3,37 +3,37 @@ using Autofac.Extensions.DependencyInjection;
 using TapeCat.Template.Api;
 
 var builder_ = WebApplication.CreateBuilder (
-    options: new ()
-    {
-        Args = args ,
-        WebRootPath = "webroot"
-    } );
+	options: new ()
+	{
+		Args = args ,
+		WebRootPath = "webroot"
+	} );
 
 var startup_ = new Startup ( builder_.Configuration , builder_.Environment );
 
 builder_.Host
-    .UseServiceProviderFactory ( new AutofacServiceProviderFactory () )
-    .ConfigureContainer<ContainerBuilder> ( startup_.ConfigureContainer )
-    .ConfigureAppConfiguration ( ( hostBuilderContext , config ) =>
-    {
-        config
-            .AddJsonFile (
-                path: "./appsettings.json" ,
-                optional: false ,
-                reloadOnChange: true )
+	.UseServiceProviderFactory ( new AutofacServiceProviderFactory () )
+	.ConfigureContainer<ContainerBuilder> ( startup_.ConfigureContainer )
+	.ConfigureAppConfiguration ( ( hostBuilderContext , config ) =>
+	{
+		config
+			.AddJsonFile (
+				path: "./appsettings.json" ,
+				optional: false ,
+				reloadOnChange: true )
 
-            .AddJsonFile (
-                path: $"./appsettings.{hostBuilderContext.HostingEnvironment.EnvironmentName}.json" ,
-                optional: true ,
-                reloadOnChange: true )
+			.AddJsonFile (
+				path: $"./appsettings.{hostBuilderContext.HostingEnvironment.EnvironmentName}.json" ,
+				optional: true ,
+				reloadOnChange: true )
 
-            .AddEnvironmentVariables ();
-    } );
+			.AddEnvironmentVariables ();
+	} );
 
 startup_.ConfigureServices ( builder_.Services );
 
-var webApplication = builder_.Build ();
+var webApplication_ = builder_.Build ();
 
-startup_.Configure ( webApplication );
+startup_.Configure ( webApplication_ );
 
-await webApplication.RunAsync ();
+await webApplication_.RunAsync ();
